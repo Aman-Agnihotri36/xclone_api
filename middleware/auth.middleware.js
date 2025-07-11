da47cbbb347c842aa4382f54387f4d34d62e5c54
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-07-11 21:55:36 +0530

    fix

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,6 +1,11 @@
-export const protectRoute = async (req, res, next) => {
-    if (!req.auth().isAuthenticated) {
+import { getAuth } from "@clerk/clerk-sdk-node";
+
+export const protectRoute = (req, res, next) => {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
         return res.status(401).json({ message: "Unauthorized - you must be logged in" });
     }
+
     next();
 };
